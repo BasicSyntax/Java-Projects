4bf40463c68bcd21823495bd16117d0f6af8243d
B Spinks <beauspinks@gmail.com>
2020-05-28 20:06:30 +0100

    edits

--- Strong.java
+++ Strong.java
@@ -1,31 +1,18 @@
+
 import java.util.ArrayList;
 import java.util.Stack;
 
-public class Strong
-{
-	public static void main(String[] args)
-	{
-		int base = 1;
-		int carry = 999;
-		int check;
-		int result = 0;
-		for(int i = 1; i < 1000;i++)
-		{
-			carry = base;
-			while(carry > 0)
-			{
+public class Strong{
+	public static void main(String[] args){
+		for(int i = 1; i < 500;i++){
+			int carry = i;
+			int result = 0;
+			while(carry > 0){
 				int remain = carry % 10;
 				result = result + remain*remain*remain;
 				carry/=10;
-				
 			}
-			
-			if(base == result)
-			{
-				System.out.println(base);
-			}
-			result = 0;
-			base++;
+			if(i == result){System.out.println(i);}
 		}
 		
 	}
